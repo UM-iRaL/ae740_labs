00931550415d56402b139b6ec7f3d7699106ae72
Atharva Navsalkar <anavsalkar@gmail.com>
2025-08-22 03:40:33 -0400

    added lab1 code

---
+++ lab1/random_vector.py
@@ -0,0 +1,20 @@
+import random
+class RandomVector:
+    def __init__(self, size, max_val=1.0):
+        # TODO: Initialize a list of random values between 0 and max_val
+        pass
+    def mean(self):
+        # TODO: Calculate and return the mean
+        pass
+    def max(self):
+        # TODO: Find and return the maximum value
+        pass
+    def min(self):
+        # TODO: Find and return the minimum value
+        pass
+    def print(self):
+        # TODO: Print all values in the vector
+        pass
+    def print_histogram(self, bins):
+        # TODO: Create and print a text histogram with the specified number of bins
+        pass
